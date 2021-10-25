32713c1959726a9a5de0172b22010adfe7381b43
Lucas Bertoloto dos Santos <lucasbdoss@hotmail.com>
2021-10-25 14:00:21 -0300

    Initial commit

---
+++ src/main/java/br/com/lucasbertoloto/registradordecalculos/RegistradorDeCalculosApplication.java
@@ -0,0 +1,13 @@
+package br.com.lucasbertoloto.registradordecalculos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistradorDeCalculosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistradorDeCalculosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/lucasbertoloto/registradordecalculos/RegistradorDeCalculosApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lucasbertoloto.registradordecalculos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistradorDeCalculosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
